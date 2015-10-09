39aa64ef4e83ee33736b836033de1b543bba7af0
Felix Haller <ich@ein-freier-mensch.de>
2015-10-09 14:23:18 +0200

    Doku bearbeitet
    * Fazit verfasst
    * REST ressourcen im Überblick verfasst
    * Skizzen hinzugefügt
    * Anhang verlinkt

--- core/src/test/java/windyoak/core/impl/StoreServiceInSQLiteTest.java
+++ core/src/test/java/windyoak/core/impl/StoreServiceInSQLiteTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package windyoak.core.impl;
 
 import org.junit.After;
@@ -19,7 +14,7 @@ import windyoak.core.Users;
 
 /**
  *
- * @author felix
+ * @author Felix Haller
  */
 public class StoreServiceInSQLiteTest
 {
